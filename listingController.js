499add6ed3520bb6b3f48a9319715a2fae2bc3cf
Taariq Imami <timami@ufl.edu>
2016-01-15 22:32:26 -0500

    Finish

--- listingController.js
+++ listingController.js
@@ -1,14 +1,24 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.newListing = {};
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+        $scope.listings.push($scope.newListing);
+        $scope.newListing = {};
+    };
+    $scope.deleteListing = function(listing) {
+        var index = $scope.listings.indexOf(listing);
+        $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(listing) {
+        var index = $scope.listings.indexOf(listing);
+        $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
